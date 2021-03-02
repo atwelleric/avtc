8208f4986ad0384cb685837de8d00ac91fc53673
atwelleric <atwelleric@gmail.com>
2021-03-02 10:51:14 -0600

    added setting.sql

--- avtc_back/settings.py
+++ avtc_back/settings.py
@@ -156,4 +156,4 @@ DEFAULT_FILE_STORAGE = 'avtc_back.storages.MediaStore'
 # MEDIA_URL = '/media/'
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-django_heroku.setting(locals())
+# django_heroku.setting(locals())
